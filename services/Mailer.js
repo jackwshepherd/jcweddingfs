b5a847ea07f1399886e51d38837e0e737c215549
Jack Shepherd <jack.shepherd@leverarch.app>
2019-12-30 15:38:34 +0000

    Mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,7 +5,7 @@ sendgrid.setApiKey(keys.sendGrid);
 
 module.exports = async ({ email, name, subject, html }) => {
   const msg = {
-    to: keys.sendFakeEmails === true ?
+    to: keys.sendFakeEmails === "on" ?
        'Jack Shepherd <panoramical@gmail.com>' : `${name} <${email}>`,
     from: 'Charlie and Jack <charlieandjack11@gmail.com>',
     subject: subject,
